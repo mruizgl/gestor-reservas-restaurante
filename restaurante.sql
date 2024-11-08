01eb06ecd50baa335c7b3a0fd5e8357a9c471193
NoeClariNista <chispa1992@gmail.com>
2024-11-08 12:52:03 +0000

    fix(restaurante.sql) : Se corrige alguna información de la Base de Datos de Restaurante para el Proyecto de Gestor Reservas Restaurante de AED.

--- restaurante.sql
+++ restaurante.sql
@@ -2,7 +2,7 @@ CREATE DATABASE restaurante;
 
 use restaurante;
 
-CREATE TABLE 'clientes' (
+CREATE TABLE clientes (
     dni CHARACTER(20),
     nombre CHARACTER(50),
     apellidos CHARACTER(50),
@@ -13,24 +13,24 @@ CREATE TABLE 'clientes' (
     CONSTRAINT pk_alumnos PRIMARY KEY(dni)
 );
 
-CREATE TABLE 'usuarios' (
+CREATE TABLE usuarios (
     id int AUTO_INCREMENT,
     nombre CHARACTER(50),
     apellido CHARACTER(50),
     CONSTRAINT pk_usuarios PRIMARY KEY(id)
 );
 
-CREATE TABLE 'mesas' (
+CREATE TABLE mesas (
     id int AUTO_INCREMENT,
     tamaño int,
     CONSTRAINT pk_mesas PRIMARY KEY(id)
 );
 
-CREATE TABLE 'cliente_mesa' (
+CREATE TABLE cliente_mesa (
     id int AUTO_INCREMENT,
-    idcliente int,
+    idcliente CHARACTER(20),
     idmesa int,
     CONSTRAINT pk_cliente_mesa PRIMARY KEY(id),
-    CONSTRAINT fk_clientes FOREIGN KEY(idcliente) REFERENCES clientes(id),
-    CONSTRAINT fk_mesas FOREIGN KEY(idmesa) REFERENCES mesas(id),
+    CONSTRAINT fk_clientes FOREIGN KEY(idcliente) REFERENCES clientes(dni),
+    CONSTRAINT fk_mesas FOREIGN KEY(idmesa) REFERENCES mesas(id)
 );
