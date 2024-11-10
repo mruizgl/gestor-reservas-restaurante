b6ff517cfd601bf6360a50b4fe209f3ec6378e33
NoeClariNista <chispa1992@gmail.com>
2024-11-10 18:59:32 +0000

    fix(gestor-restaurante): Se añade alguna anotación en la vista de bienvenida.

--- gestor-restaurante/resources/views/welcome.blade.php
+++ gestor-restaurante/resources/views/welcome.blade.php
@@ -1,44 +1,44 @@
 <!DOCTYPE html>
 <html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Gestión de Restaurante</title>
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}"> 
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+    <title> Gestión de Restaurante. </title>
 </head>
-<body class="bg-gray-100">
 
+<body>
 
     <header class="bg-blue-800 text-white py-4">
         <div class="container mx-auto text-center">
-            <h1 class="text-4xl font-bold">Bienvenido a la Gestión del Restaurante</h1>
+            <h1 class="text-4xl font-bold"> Bienvenido a la Gestión del Restaurante. </h1>
         </div>
     </header>
 
-
     <main class="container mx-auto my-10">
         <div class="flex flex-col items-center space-y-6">
-            <p class="text-xl text-gray-700">Elige una opción para continuar:</p>
-
+            <p class="text-xl text-gray-700"> Elige una opción para continuar: </p>
+            </br>
             <div class="flex space-x-4">
-                <a href="{{ route('login') }}" class="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-lg">
-                    Iniciar Sesión
+                <a href="{{route('login')}}" class="bg-blue-600 hover:bg-blue-700 text-white py-2 px-4 rounded-lg">
+                    Iniciar Sesión.
                 </a>
-                
-                <a href="{{ route('reservas.index') }}" class="bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded-lg">
-                    Ver reservas
+                </br>
+                </br>
+                <a href="{{route('reservas.index')}}" class="bg-green-600 hover:bg-green-700 text-white py-2 px-4 rounded-lg">
+                    Ver reservas.
                 </a>
-
             </div>
         </div>
     </main>
 
-
     <footer class="bg-blue-800 text-white py-4">
         <div class="container mx-auto text-center">
-            <p>&copy; {{ date('Y') }} Gestión de Restaurante. Todos los derechos reservados.</p>
+            <p> &copy; {{date('Y')}}. Gestión de Restaurante. Todos los derechos reservados. </p>
         </div>
     </footer>
 
 </body>
+
 </html>
