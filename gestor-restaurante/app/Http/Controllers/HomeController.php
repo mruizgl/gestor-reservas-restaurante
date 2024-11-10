1388add7dc7f439ad20b3e69380e739b3ab81e6f
NoeClariNista <chispa1992@gmail.com>
2024-11-10 18:58:29 +0000

    fix(gestor-restaurante): Se actualiza el controlador para el Home.

--- gestor-restaurante/app/Http/Controllers/HomeController.php
+++ gestor-restaurante/app/Http/Controllers/HomeController.php
@@ -3,12 +3,26 @@
 namespace App\Http\Controllers;
 
 use App\Http\Controllers\Controller;
-use App\Models\User; 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
+use Carbon\Carbon;
 
-class HomeController extends Controller
-{
-    
+/**
+ * Controlador de la home de empleados y administradores de la app
+ * @author Melissa y Noelia
+ */
+
+class HomeController extends Controller {
+
+    public function indexHome() {
+        return view('home.index');
+    }
+
+   public function home() {
+        dd('Usuario creado correctamente.');
+        return redirect()->route('home');
+    }
+
 }
