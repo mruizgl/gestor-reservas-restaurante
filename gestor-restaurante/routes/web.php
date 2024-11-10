9df22d9c97cb672a5fb0db2c5f97915b21b492bd
NoeClariNista <chispa1992@gmail.com>
2024-11-10 18:56:21 +0000

    fix(gestor-restaurante): Se actualiza la ruta de web.php añadiendo la ruta de /crearEmpleados y actualizando las otras rutas que ya estaban creadas.

--- gestor-restaurante/routes/web.php
+++ gestor-restaurante/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ReservaController;
+use App\Http\Controllers\FormularioController;
+use App\Http\Controllers\HomeController;
 
 Route::get('/', function () {
     return view('welcome');
@@ -10,5 +12,11 @@ Route::get('/', function () {
 
 Route::get('/login', [AuthController::class, 'showLoginForm'])->name('auth.login');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
+
+Route::get('/crearEmpleado', [FormularioController::class, 'CrearEmpleadoIndex'])->name('crearEmpleado.index');
+Route::post('/crearEmpleado', [FormularioController::class, 'formulario'])->name('formulario');
+
+Route::get('/home', [HomeController::class, 'indexHome'])->name('home.index');
+Route::post('/home', [HomeController::class, 'home'])->name('home');
 
 Route::get('/reservas', [ReservaController::class, 'index'])->name('reservas.index');
