9bc671bf5c48b1a7d899a06c3451963234838ae7
NoeClariNista <chispa1992@gmail.com>
2024-11-12 20:08:55 +0000

    fix(gestor-restaurante): Se actualiza el repositorio.

---
+++ gestor-restaurante/resources/views/reservas/index.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title> Reservas. </title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
+</head>
+<body>
+    <header class="bg-blue-800 text-white py-4">
+        <h1> Reservas. </h1>
+    </header>
+
+    <main class="container mx-auto my-10">
+
+    </main>
+
+    <footer class="bg-blue-800 text-white py-4">
+        <div class="container mx-auto text-center">
+            <p>&copy; {{date('Y')}}. Gestión de Restaurante. Todos los derechos reservados.</p>
+        </div>
+    </footer>
+
+</body>
+</html>
