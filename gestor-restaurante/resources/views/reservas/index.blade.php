c4458046eb87dfe49f477414dcb6c20cf21d100b
NoeClariNista <chispa1992@gmail.com>
2024-11-12 21:38:04 +0000

    fix(gestor-restaurante): Se actualiza la pantalla principal de las reservas.

--- gestor-restaurante/resources/views/reservas/index.blade.php
+++ gestor-restaurante/resources/views/reservas/index.blade.php
@@ -13,7 +13,34 @@
     </header>
 
     <main class="container mx-auto my-10">
-
+        <form>
+            @csrf
+            <label for="fecha"> Fecha reserva: </label>
+            <input type="date" id="fecha" name="fecha" required>
+            </br>
+            <label for="numeroOcupantes"> Numero ocupantes: </label>
+            <input type="number" id="numeroOcupantes" name="numeroOcupantes" min="1" max="6" required>
+            </br>
+            <label for="nombre"> Nombre: </label>
+            <input type="text" id="nombre" name="nombre" required>
+            </br>
+            <label for="telefono"> Telefono: </label>
+            <input type="text" id="telefono" name="telefono" required>
+            </br>
+            <label for="zona"> Zona: </label>
+            <select name="zona" id="zona">
+                <option> Interior </option>
+                <option> Terraza </option>
+            </select>
+            </br>
+            <button type="submit"> Pagina Canvas. </button>
+            </br>
+            @if ($errors->any())
+                <div>
+                    <strong> Error. </strong>
+                </div>
+            @endif
+        </form>
     </main>
 
     <footer class="bg-blue-800 text-white py-4">
