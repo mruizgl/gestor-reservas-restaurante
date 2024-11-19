5fa04e53deda456e815304bced71ca10836b48a4
NoeClariNista <chispa1992@gmail.com>
2024-11-19 22:21:57 +0000

    fix(gestor-restaurante): Se actualiza el controlador de añadir mesas.

--- gesto-rest/app/Http/Controllers/AddTablesController.php
+++ gesto-rest/app/Http/Controllers/AddTablesController.php
@@ -15,14 +15,14 @@ class AddTablesController extends Controller
         $zona = $request->zona;
         $dia = $request->dia;
         $capacidad = $request->capacidad;
-        return view('addTables.addTables'
-        )->with('zona', $zona)->with('dia', $dia)->with('capacidad', $capacidad);
+        return view('addTables.addTables')->with('zona', $zona)->with('dia', $dia)->with('capacidad', $capacidad);
     }
 
     public function preview(Request $request) {
+        $zona = $request->zona;
         $dia = $request->dia;
         $capacidad = $request->capacidad;
-        return view('addTables.preview')->with('dia', $dia)->with('capacidad', $capacidad);
+        return view('addTables.preview')->with('zona', $zona)->with('dia', $dia)->with('capacidad', $capacidad);
     }
 
 /*    public function back() {
